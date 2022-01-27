da392d5346d3d1858daae26898f732e16fc7664b
zhiying <wangzy446@chinaunicom.cn>
2022-01-27 16:38:28 +0800

    工单管理页面修改

--- src/pages/WorkOrderManage/Order/index.tsx
+++ src/pages/WorkOrderManage/Order/index.tsx
@@ -1,14 +1,16 @@
-
 import React, {Component} from 'react';
-import { history } from 'ice';
-import {Button, Form, Input, Select, message, Modal} from 'antd';
-const { Option } = Select;
-import { UserOutlined, LockOutlined,SearchOutlined } from '@ant-design/icons';
+import {history} from 'ice';
+import Highcharts from 'highcharts';
+import {Button, Form, Input, Select, message, Modal, DatePicker} from 'antd';
+
+const {RangePicker} = DatePicker;
+const {Option} = Select;
+import {UserOutlined, LockOutlined, SearchOutlined} from '@ant-design/icons';
 import PageTitle from '@/components/PageTitle';
 import SCTable from '@/components/Table';
 import styles from './index.module.scss';
-import { FormInstance } from 'antd/es/form';
-
+import {FormInstance} from 'antd/es/form';
+import style from "@/pages/FreeManage/index.module.scss";
 
 
 export default class Order extends Component {
@@ -17,21 +19,19 @@ export default class Order extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      isLoading:true,
-      selectRow:{},
-      tableConfig:{
-        isShowSelect:false,
-        multipleSelectionType:'checkbox',
-        isShowPagination:true
+      isLoading: true,
+      selectRow: {},
+      tableConfig: {
+        isShowSelect: false,
+        multipleSelectionType: 'checkbox',
+        isShowPagination: true
       },
-      tableData:[],
-      totalSize:0,
-      currentPage:1,
+      tableData: [],
+      totalSize: 0,
+      currentPage: 1,
     }
   }
-  componentDidMount() {
-    this.query();
-  }
+
   columns = [
     {
       title: '工单编号',
@@ -78,60 +78,92 @@ export default class Order extends Component {
 
 
   ];
-  query = (pageNum,pageSize) =>{
+  query = (pageNum, pageSize) => {
 
-    this.setState({isLoading:true});
+    this.setState({isLoading: true});
     let that = this;
-    let params =  {
-      index:pageNum == undefined ? 1 :pageNum,
-      rows:pageSize == undefined ? 10 :pageSize,
+    let params = {
+      index: pageNum == undefined ? 1 : pageNum,
+      rows: pageSize == undefined ? 10 : pageSize,
     };
-    setTimeout(()=>{
+    setTimeout(() => {
       let data = [{
-        "orderId":"JSFW1233",
-        "orderName":"无法远程连接ECS实例",
-        "orderType":"技术服务",
-        "prdType":"云服务器 ECS",
-        "createTime":"2021-08-01 20:46:25",
-        "status":"待处理",
-        "appUser":"XXXX",
+        "orderId": "JSFW1233",
+        "orderName": "无法远程连接ECS实例",
+        "orderType": "技术服务",
+        "prdType": "云服务器 ECS",
+        "createTime": "2021-08-01 20:46:25",
+        "status": "待处理",
+        "appUser": "XXXX",
       }];
-      that.setState({isLoading:false,tableData:data,totalSize:22,currentPage:params.index});
-    },3000)
+      that.setState({isLoading: false, tableData: data, totalSize: 22, currentPage: params.index});
+    }, 3000)
 
   };
-  changePageFn = (pageNum,pageSize) =>{
-    console.log(pageNum,pageSize);
-    this.query(pageNum,pageSize);
+  changePageFn = (pageNum, pageSize) => {
+    console.log(pageNum, pageSize);
+    this.query(pageNum, pageSize);
   };
 
-  selectedRowFn = (row) =>{
+  selectedRowFn = (row) => {
     console.log(row);
-    this.setState({selectRow:row})
+    this.setState({selectRow: row})
   };
 
-  reset = () =>{
+  reset = () => {
     this.formRef.current!.resetFields();
   };
 
-
+  draw = () => {
+    var chart = Highcharts.chart('container', {
+      chart: {},
+      title: {
+        floating: true,
+        text: ''
+      },
+      plotOptions: {
+        pie: {
+          allowPointSelect: true,
+          cursor: 'pointer',
+          dataLabels: {
+            enabled: false
+          },
+          //showInLegend: true
+        }
+      },
+      colors: ['rgba(255, 102, 102, 1)', 'rgba(227, 213, 37, 1)'],
+      credits: {
+        enabled: false
+      },
+      series: [{
+        type: 'pie',
+        innerSize: '60%',
+        name: '市场份额',
+        data: [
+          {name: 'Firefox', y: 45.0},
+          ['IE', 26.8],
+        ]
+      }]
+    });
+  };
 
   render() {
     const layout = {
-      labelCol: { span: 8 },
-      wrapperCol: { span: 16 },
+      labelCol: {span: 8},
+      wrapperCol: {span: 16},
     };
     const tailLayout = {
-      wrapperCol: { offset: 0, span: 24 },
+      wrapperCol: {offset: 0, span: 24},
     };
     return (
       <div className={styles['order-content']}>
-        <PageTitle title="全部工单" />
+        <PageTitle title="全部工单"/>
         <div className={styles['order-info']}>
           <div className={styles['order-num']}>
             <div>工单总数</div>
             <div>10</div>
           </div>
+          <div id="container" style={{minWidth: '100px', height: '100px'}}></div>
           <div className={styles['order-type']}>
             <div className={styles['order-item']}>
               <label>产品发布</label>
@@ -141,22 +173,36 @@ export default class Order extends Component {
               </div>
             </div>
             <div className={styles['order-item']}>
-              <label>技术服务</label>
+              <label>技术支持</label>
               <div className={styles['order-show']}>
-                <p>2</p>
-                <div>0%</div>
+                <p>5</p>
+                <div>29%</div>
               </div>
-
             </div>
-
+          </div>
+          <div className={styles['order-type']}>
+            <div className={styles['order-item']}>
+              <label>产品发布超时工单数量</label>
+              <div className={styles['order-show']}>
+                <p>5</p>
+                <div>41%</div>
+              </div>
+            </div>
+            <div className={styles['order-item']}>
+              <label>技术支持超时工单数量</label>
+              <div className={styles['order-show']}>
+                <p>4</p>
+                <div>80%</div>
+              </div>
+            </div>
           </div>
         </div>
 
         <div className={'search-content'}>
           <Form {...layout} ref={this.formRef} name="search-content" className={'searchForm-content'}>
             <div className={'searchForm-item'}>
-              <Form.Item name="organ" label="组织机构">
-                <Input />
+              <Form.Item name="organ" label="时间范围：">
+                  <RangePicker picker="month"/>
               </Form.Item>
             </div>
             <div className={'searchForm-item'}>
@@ -185,8 +231,8 @@ export default class Order extends Component {
             </div>
             <div className={'searchForm-operate'}>
               <Form.Item {...tailLayout}>
-                <Button type="primary" htmlType="submit" >查询</Button>
-                <Button type="primary" onClick={()=>this.reset()}>重置</Button>
+                <Button type="primary" htmlType="submit">查询</Button>
+                <Button type="primary" onClick={() => this.reset()}>重置</Button>
               </Form.Item>
             </div>
           </Form>
@@ -194,8 +240,8 @@ export default class Order extends Component {
 
         <div className={'table-content'}>
           <div className={'table-operateBtn-content'}>
-            <Button icon={<SearchOutlined />}>创建角色</Button>
-            <Button icon={<SearchOutlined />}>批量删除</Button>
+            <Button icon={<SearchOutlined/>}>创建角色</Button>
+            <Button icon={<SearchOutlined/>}>批量删除</Button>
           </div>
           <div className={'table-layout'}>
             <SCTable
@@ -211,9 +257,13 @@ export default class Order extends Component {
         </div>
 
 
-
       </div>
     );
+  }
+
+  componentDidMount() {
+    this.query();
+    this.draw();
   }
 }
 
--- src/pages/WorkOrderManage/TechSupportOrder/index.tsx
+++ src/pages/WorkOrderManage/TechSupportOrder/index.tsx
@@ -1,9 +1,13 @@
 
 import React, {Component} from 'react';
 import { history } from 'ice';
-import {Button, Form, Input, Checkbox, message, Modal} from 'antd';
-import { UserOutlined, LockOutlined } from '@ant-design/icons';
+import {Button, Form, Input, Select, message, Modal} from 'antd';
+const { Option } = Select;
+import { UserOutlined, LockOutlined,SearchOutlined } from '@ant-design/icons';
 import styles from './index.module.scss';
+import PageTitle from '@/components/PageTitle';
+import SCTable from '@/components/Table';
+import DisabledTenant from "@/pages/TenantManage/Tenant/modal/Disabled";
 
 
 
@@ -11,17 +15,193 @@ export default class TechSupportOrder extends Component {
   constructor(props) {
     super(props);
     this.state = {
+      isLoading:true,
+      selectRow:{},
+      tableConfig:{
+        isShowSelect:false,
+        multipleSelectionType:'checkbox',
+        isShowPagination:true
+      },
+      tableData:[],
+      totalSize:0,
+      currentPage:1,
     }
   }
   componentDidMount() {
+    this.query();
   }
+
+  columns = [
+    {
+      title: '租户名称/ID',
+      dataIndex: 'tenantName',
+      key: 'tenantName',
+      ellipsis: true,
+    },
+    {
+      title: '组织机构',
+      dataIndex: 'organName',
+      key: 'organName',
+      ellipsis: true,
+    },
+    {
+      title: '姓名',
+      dataIndex: 'name',
+      key: 'name',
+      ellipsis: true,
+    },
+    {
+      title: '手机号码',
+      dataIndex: 'phone',
+      key: 'phone',
+      ellipsis: true,
+    },
+    {
+      title: '邮箱',
+      dataIndex: 'email',
+      key: 'email',
+      ellipsis: true,
+    },
+    {
+      title: '状态',
+      dataIndex: 'status',
+      key: 'status',
+      ellipsis: true,
+    },
+    {
+      title: '备注',
+      dataIndex: 'remark',
+      key: 'remark',
+      ellipsis: true,
+    },
+    {
+      title: '操作',
+      key: 'action',
+      render: (text, record) => (
+        <div>
+          {
+            record.status == 1? <a onClick={()=>this.useOrDisabled(record)}></a>:<a  onClick={()=>this.useOrDisabled(record)}>禁用</a>
+          }
+
+        </div>
+      ),
+    },
+
+  ];
+  query = (pageNum,pageSize) =>{
+
+    this.setState({isLoading:true});
+    let that = this;
+    let params =  {
+      index:pageNum == undefined ? 1 :pageNum,
+      rows:pageSize == undefined ? 10 :pageSize,
+    };
+    setTimeout(()=>{
+      let data = [{
+        "tenantName":"测试租户",
+        "tenantId":"2275687700232037392",
+        "organName":"XX公司/XX部门/XX小组",
+        "name":"张三",
+        "phone":"15611118086",
+        "email":"43224288@qq.com",
+        "status":"正常",
+        "remark":"成功",
+      }];
+      that.setState({isLoading:false,tableData:data,totalSize:22,currentPage:params.index});
+    },3000)
+
+  };
+  changePageFn = (pageNum,pageSize) =>{
+    console.log(pageNum,pageSize);
+    this.query(pageNum,pageSize);
+  };
+
+  selectedRowFn = (row) =>{
+    console.log(row);
+    this.setState({selectRow:row})
+  };
+  useOrDisabled = (record) =>{
+    this.modal = Modal.info({
+      title:"禁用",
+      closable:true,
+      okText:"确定",
+      cancelText:"取消",
+      centered:true,
+      width:600,
+      //icon:"none",
+      content:<DisabledTenant submitFn={this.submitFn} cancelFn={this.cancelFn}></DisabledTenant>,
+      onOk:()=>{
+      },
+      onCancel:()=>{
+      }
+    })
+  };
+
+  submitFn = (value) =>{
+    console.log(value);
+    message.success('操作成功！');
+    this.modal.destroy();
+  };
+  cancelFn = () =>{
+    message.error('操作失败！');
+    this.modal.destroy();
+  };
+
+
 
 
 
   render() {
+    const layout = {
+      labelCol: { span: 8 },
+      wrapperCol: { span: 16 },
+    };
+    const tailLayout = {
+      wrapperCol: { offset: 0, span: 24 },
+    };
     return (
-      <div className={styles['techSupportOrder-content']}>
-        <div>techSupportOrder content</div>
+      <div className={styles['tenant-content']}>
+        <PageTitle title="技术支持工单" />
+        <div className={'search-content'}>
+          <Form {...layout} name="search-content" className={'searchForm-content'}>
+            <div className={'searchForm-item'}>
+              <Form.Item name="status" label="产品类型">
+                <Select placeholder="请选择" allowClear>
+                  <Option value="success">正常</Option>
+                  <Option value="stop">停用</Option>
+                </Select>
+              </Form.Item>
+            </div>
+            <div className={'searchForm-item'}>
+              <Form.Item name="status" label="工单状态">
+                <Select placeholder="请选择" allowClear>
+                  <Option value="success">正常</Option>
+                  <Option value="stop">停用</Option>
+                </Select>
+              </Form.Item>
+            </div>
+            <div className={'searchForm-operate'}>
+              <Form.Item {...tailLayout}>
+                <Button type="primary" htmlType="submit" >查询</Button>
+                <Button type="primary" htmlType="submit">重置</Button>
+              </Form.Item>
+            </div>
+          </Form>
+        </div>
+
+        <div className={'table-content'}>
+          <div className={'table-layout'}>
+            <SCTable
+              isLoading={this.state.tisLoading}
+              columns={this.columns}
+              dataSource={this.state.tableData}
+              tableConfig={this.state.tableConfig}
+              totalSize={this.state.totalSize}
+              current={this.state.currentPage}
+              changePage={this.changePageFn}
+              selectedRow={this.selectedRowFn}/>
+          </div>
+        </div>
 
 
       </div>
